479d2793deebb3508155a35344f479d7bd5184b2
sherrillmix <sherrillmix@users.noreply.github.com>
2018-02-28 08:40:40 -0500

    Setting up bamtoalign

--- dnapy/bamtoalign.py
+++ dnapy/bamtoalign.py
@@ -5,14 +5,21 @@ from dnapy import helper
 
 
 
-def getStartsInFile(inputFile,region=None,maxGaps=0):
+def getAlignsInFile(inputFile,region=None):
     with pysam.AlignmentFile(inputFile, "rb" ) as samfile:
-        #print(samfile.pileup(region=region))
         for read in samfile.fetch(region=region):
+            rPos=read.reference_start
+            qPos=0
+            for operation,length in read.cigartuples:
+                print operation
+                print length
+                if operation in [0,1,4,7,8]:
+                    qPos+=length
+                    print qPos
+            print read.query_sequence
+            print read.get_reference_positions()
             #filter here
-            gaps=sum([length for op, length in read.cigar if op in [1,2,3]]) #insertion,deletion,refskip
-            if gaps<=maxGaps:
-                yield {"ref": read.reference_name, "start": read.pos+1, "end": read.aend,'strand': '-' if read.flag&16==16 else '+'}
+            yield {"name": read.query_name, "start": read.pos+1, "end": read.aend,'strand': '-' if read.flag&16==16 else '+'}
 
 
 
@@ -21,7 +28,7 @@ def main(argv=None):
     parser.add_argument('bamFile', help='a bam file containing the alignment',type=helper.checkFile)
     parser.add_argument("-v","--verbose", help="increase output verbosity to stderr", action="store_true")
     parser.add_argument("-r","--region", help="the region to pull reads from",default=None)
-    parser.add_argument("-e","--endSpan", help="Ignore spans of matches at the start or end of a read less than this cutoff",default=0,type=int)
+    parser.add_argument("-e","--endSpan", help="ignore spans of matches at the start or end of a read less than this cutoff",default=0,type=int)
     args=parser.parse_args(argv)
  
         
@@ -30,23 +37,9 @@ def main(argv=None):
         for key, value in vars(args).items():
             sys.stderr.write("   "+key+": "+str(value))
 
-    if args.file is None:
-        regions=[args.region]
-    else:
-        with open(args.file) as f:
-            regions=[x.strip('\n') for x in f.readlines()]
-
-
-    if(not args.noHeader):
-        if args.regionColumn: header="region,ref,start,end,strand"
-        else: header="ref,start,end,strand"
-        print(header)
-    for region in regions:
-        for read in getStartsInFile(args.bamFile,region,args.maxGaps):
-            if args.regionColumn:
-                print("%s,%s,%d,%d,%s" % (region,read["ref"],read['start'],read['end'],read['strand']))
-            else:
-                print("%s,%d,%d,%s" % (read["ref"],read['start'],read['end'],read['strand']))
+    for read in getAlignsInFile(args.bamFile,args.region):
+        print read
+        break
             
 
     
