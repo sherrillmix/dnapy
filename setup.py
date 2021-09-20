e0435c2638fba49190ffd110bc6ec4007e50b4ec
sherrillmix <sherrillmix@users.noreply.github.com>
2021-09-20 10:28:59 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 setup(name='dnapy',
-    version='0.1.5',
+    version='0.1.6',
     description='Some handy functions for dealing with sequence alignment files.',
     url='http://github.com/sherrillmix/dnapy',
     author='Scott Sherrill-Mix',
