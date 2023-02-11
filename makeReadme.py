df6ed2bf51fffef790f5239ecc5bf9eff8ba6ca2
sherrillmix <sherrillmix@users.noreply.github.com>
2023-02-11 00:55:52 -0500

    Convert to utf8

--- makeReadme.py
+++ makeReadme.py
@@ -6,7 +6,9 @@ import re
 entries=pkg_resources.get_entry_map('dnapy',None)['console_scripts'].keys()
 
 helps=[subprocess.check_output(x+' --help;exit 0',stderr=subprocess.STDOUT,shell=True) for x in entries]
-inserts=['\n'.join([head,"~~~~","","::",'  ']+['  '+x for x in help.split('\n')]) for head,help in zip(entries,helps)]
+print(entries)
+#print(helps)
+inserts=['\n'.join([head,"~~~~","","::",'  ']+['  '+x for x in help.decode('utf8').split('\n')]) for head,help in zip(entries,helps)]
 
 insert='\n'.join(inserts)
 
